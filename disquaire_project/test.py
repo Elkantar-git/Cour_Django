1f593b1062e752a1d717374afcac4a08ba7902f5
Elkantar-git <79974319+Elkantar-git@users.noreply.github.com>
2021-10-31 23:28:05 +0100

    maj test.py

--- disquaire_project/test.py
+++ disquaire_project/test.py
@@ -1,3 +1,5 @@
+import re
+
 txt = open("album.txt",'r')
 list = txt.read().split('\n')
 txt.close()
@@ -9,13 +11,12 @@ linkr = open("return_link.txt",'w')
 for i in range(0, int(len(list)/4)):
     x = list[i*4:(i+1)*4]
 
-    art = x[1].replace(" ", "").replace("'", "")
-    alb = x[2].replace(" ", "").replace("'", "")
+    art = re.sub("[ ']", "", x[1])
+    alb = re.sub("[ ']", "", x[2])
 
     artist = '{} = Artist.objects.create(name="{}")\n'.format(art, x[1])
     album = '{} = Album.objects.create(title="{}", picture="{}", pk={})\n'.format(alb, x[2], x[3], x[0])
     link = '{}.artists.add({})\n'.format(alb, art)
-
 
     artistr.write(artist)
     albumr.write(album)
