aa497d7e47cc2e400ed326b2aad6e36b148fc02e
Elkantar-git <79974319+Elkantar-git@users.noreply.github.com>
2021-11-01 00:31:23 +0100

    Erreur runserver

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,30 +1,33 @@
-# from django.http.response import HttpResponse
-# from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render
 from .models import Album, Artist, Contact, Booking
-
-from django.template import loader
 
 
 def index(request):
     albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
-    template = loader.get_template('store/index.html')
-    context = {'albums': albums}
-    return HttpResponse(template.render(context, request=request))
+    context = {
+        'albums': albums
+    }
+    return render(request, 'store/index.html', context)
 
 def listing(request):
     albums = Album.objects.filter(available=True)
-    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
-    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-    return HttpResponse(message)
+    context = {
+        'albums': albums
+    }
+    return render(request, 'store/listing.html', context)
 
 
 def detail(request, album_id):
     album = Album.objects.get(pk=album_id)
     artists = " ".join([artist.name for artist in album.artists.all()])
-    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album.title, artists)
-    return HttpResponse(message)
+    artists_name = " ".join(artists)
+    context = {
+        'album_title': album.title,
+        'artists_name': artists_name,
+        'album_id': album.id,
+        'thumbnail': album.picture
+    }
+    return render(request, 'store/detail.html', context)
 
 
 def search(request):
@@ -42,10 +45,10 @@ def search(request):
 
         else:
             albums = ["<li>{}</li>".format(album.title) for album in albums]
-            message = """
-                Nous avons trouvé les albums correspondant à votre requête ! Les voici :
-                <ul>
-                    {}
-                </ul>
-            """.format("</li><li>".join(albums))
-    return HttpResponse(message)
+
+    title = "Résultats pour la requête %s"%query
+    context = {
+        'albums': albums,
+        'title': title
+    }
+    return render(request, 'store/search.html', context)
